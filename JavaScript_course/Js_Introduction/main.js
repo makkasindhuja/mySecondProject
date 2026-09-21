c879d366b941bff60f86b34d855dcded67168f96
Sindhuja Makka <sindhujamakka@Sindhujas-MacBook-Pro.local>
2026-09-21 08:55:57 -0500

    push

---
+++ JavaScript_course/Js_Introduction/main.js
@@ -0,0 +1,116 @@
+console.log("Start small. Ship something.");
+var age;
+let name;
+// declare variable num
+let num;
+// assign 5 to num 
+num = 5;
+// declare varible num1 and assign 5 to it
+let num1 = 5;
+// declare variable num2 and assign 6 to it
+let num2 = 6;
+// assign 5 to variable score
+let score = 5;
+console.log(score); // 5
+// change the value of score to 3
+score = 3;
+console.log(score); // 3
+// valid
+let message = "hello";
+let _messaage = "hello";
+let $message = "hello";
+let userage = 23;
+let Age = 20;
+console.log(userage); // 23
+console.log(Age); // 20
+let firstName = "Sindhuja";
+let lastName = "Makka";
+console.log(firstName);
+console.log(lastName);
+ message = "Hello JavaScript";
+ console.log(message)
+console.log("Good Morning");
+console.log(2000)
+const greet = "Hello";
+console.log(greet);
+let count = 5;
+console.log("There are %d items in your basket.", count);
+let message2 = "There are %d items in your basket.";
+console . log(message2);
+let fruit = "apple"
+console . log(fruit)
+let country ="USA"
+console . log(country);
+let result = "fail";
+console . log(result);
+let integer_number = -3;
+console .log(integer_number);
+let float_number = 3.15;
+console.log(float_number);
+let dataChecked = true;
+console.log(dataChecked);
+let valueCounted = false;
+console.log(valueCounted);
+let number = null;
+console.log(number);
+let value1 = Symbol("programiz");
+
+let value2 = Symbol("programiz");
+
+console.log(value1 === value2);
+let student ={
+    firstName: "John",
+    lastName: null,
+    class: 10
+};
+let x = 5;
+console.log("Addition: x + 3 = ", x + 3);
+console.log("Subtraction: x - 3 =", x - 3);
+console.log("Multiplication: x * 3 =", x * 3);
+console.log("Division: x / 3 =", x / 3);
+console.log("Remainder: x % 3 =", x % 3);
+console.log("Increment:  ++x =", ++x);
+console.log("Decrement: --x =", --x);
+console.log("Exponentitation: x ** 3 =", x ** 3);
+let a = 7;
+console.log("Assignment: a = 7, a =", a);
+a += 5;  // a = a + 5
+console.log("Addition Assignment: a += 5, a =", a);
+a -= 5;  // a = a - 5
+console.log("Subtraction Assignment: a -= 5, a =", a);
+a *= 2;  // a = a * 2
+console.log("Multiplication Assignment: a *= 2, a =", a);
+a /= 2;  // a = a / 2
+console.log("Division Assignment: a /= 2, a =", a);
+a %= 2;  // a = a % 2
+console.log("Remainder Assignment: a %= 2, a =", a);
+a **= 2;  // a = a**2
+console.log("Exponentiation Assignment: a **= 7, a =", a);
+console.log("Equal to: 2 == 2 is", 2 == 2);
+console.log("Not equal to: 3 != 3 is", 3 != 3);
+console.log("Strictly equal to: 2 === '2' is", 2 === '2');
+console.log("Strictly not equal to: 2 !== '2' is", 2 !== '2');
+console.log("Greater than: 3 > 3 is", 3 > 3);
+console.log("Less than: 2 > 2 is", 2 > 2);
+console.log("Greater than or equal to: 3 >= 3 is", 3 >= 3);
+console.log("Less than or equal to: 2 <= 2 is", 2 <= 2);
+let x2 = 3;
+console.log((x2 < 5) && (x2 > 0));
+console.log((x2 < 5) && (x2 > 6));
+console.log((x2 > 2) || (x2 > 5));
+console.log((x2 > 3) || (x2 < 0));
+console.log(!(x2 == 3));
+console.log(!(x2 < 2));
+console.log("programiz");
+name = "Sindhuja";
+console.log("Hello " + name);
+let numberOfStudents = 50;
+
+console.log(numberOfStudents);
+let result1;
+result1 = "3" + 2;
+console.log(result1, "_", typeof(result1));
+result1 = "3" + true;
+console.log(result1, "_", typeof(result1));
+result1 = "3" + null;
+console.log(result1, "_", typeof(result1));
